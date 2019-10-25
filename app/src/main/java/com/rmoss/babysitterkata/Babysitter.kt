cdf90b062a217084f2cbfd51579e1d7bda5426ca
Riley-Pillar Mac <rmoss@pillartechnology.com>
2019-10-25 13:02:41 -0500

    commit_message

--- app/src/main/java/com/rmoss/babysitterkata/Babysitter.kt
+++ app/src/main/java/com/rmoss/babysitterkata/Babysitter.kt
@@ -1,8 +1,8 @@
 package com.rmoss.babysitterkata
 
 class Babysitter {
+    // Test for bash commit
     fun calculatePay(hoursWorked: Int, hourlyRate: Int): Int {
         return hoursWorked * hourlyRate
     }
-
 }
