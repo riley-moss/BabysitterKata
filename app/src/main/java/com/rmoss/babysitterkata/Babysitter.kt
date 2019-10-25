de4645b7cc140721d13bf810be750a6c09b1fc92
Riley Moss <riley.moss@accenture.com>
2019-10-25 13:28:22 -0500

    commit_message
    Signed-off-by: Natalie Frecka <natalie.frecka@accenture.com>

--- app/src/main/java/com/rmoss/babysitterkata/Babysitter.kt
+++ app/src/main/java/com/rmoss/babysitterkata/Babysitter.kt
@@ -1,6 +1,7 @@
 package com.rmoss.babysitterkata
 
 class Babysitter {
+    // Test for bash commit
     fun calculatePay(hoursWorked: Int, hourlyRate: Int): Int {
         return hoursWorked * hourlyRate
     }
